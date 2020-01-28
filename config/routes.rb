c0a668c0efbf90f557b00115ea56f82c86944319
msimon42 <msimon@uab.edu>
2020-01-27 23:42:27 -0700

    Add routes for index and show for all resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :customer, only: [:index, :show]
+      resources :invoice_item, only: [:index, :show]
+      resources :invoice, only: [:index, :show]
+      resources :item, only: [:index, :show]
+      resources :merchant, only: [:index, :show]
+      resources :transaction, only: [:index, :show]
+    end
+  end     
 end
