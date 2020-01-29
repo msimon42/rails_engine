23722e7350466f8d2a610f3beef09524e7bde415
msimon42 <msimon@uab.edu>
2020-01-28 22:19:00 -0700

    Add route for most revenue

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get 'merchants/most_revenue', to: 'merchants#most_revenue'
+
       resources :customers, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :merchants, only: [:index, :show] do
         get '/items', to: 'merchant_items#index'
+        get '/invoices', to: 'merchant_invoices#index'
       end
       resources :transactions, only: [:index, :show]
     end
