61e9e5d67042c114378a7857067d187b0e980912
msimon42 <msimon@uab.edu>
2020-01-28 22:36:40 -0700

    Remove obsolete method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,10 +4,6 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :customers, through: :invoices
 
-  def revenue
-    invoice_items.sum('unit_price * quantity')
-  end
-
   def self.top_n_by_revenue(n)
     select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) AS revenue')
           .joins(:invoice_items)
