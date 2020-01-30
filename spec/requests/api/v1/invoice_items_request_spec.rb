b2bd3f879431f282edaf5292a87f4827ebfa9abf
msimon42 <msimon@uab.edu>
2020-01-30 10:53:59 -0700

    Add tests for invoice_item relationships

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -1,26 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe 'invoice_items api' do
-  before :each do
-    @invoice_items = create_list :invoice_item, 10
+  describe 'index show' do
+    before :each do
+      @invoice_items = create_list :invoice_item, 10
+    end
+
+    it 'can get all invoice_items' do
+      get "/api/v1/invoice_items"
+
+      expect(response).to be_successful
+      invoice_items = JSON.parse(response.body)
+
+      expect(@invoice_items.length).to eq(10)
+    end
+
+    it 'can get an individual' do
+      get "/api/v1/invoice_items/#{@invoice_items[0].id}"
+      expect(response).to be_successful
+      invoice_item = JSON.parse(response.body)
+
+      expect(invoice_item['data']['attributes']['quantity']).to eq(@invoice_items[0].quantity)
+      expect(invoice_item['data']['id']).to eq(@invoice_items[0].id.to_s)
+    end
   end
 
-  it 'can get all invoice_items' do
-    get "/api/v1/invoice_items"
+  describe 'relationships' do
+    before :each do
+      @invoice = create :invoice
+      @item = create :item
+      @invoice_item = create :invoice_item, invoice: @invoice, item: @item
+    end
 
-    expect(response).to be_successful
-    invoice_items = JSON.parse(response.body)
+    it 'can get invoice' do
+      get "/api/v1/invoice_items/#{@invoice_item.id}/invoice"
 
-    expect(@invoice_items.length).to eq(10)
-  end
+      expect(response).to be_successful
+      data = JSON.parse(response.body)
 
-  it 'can get an individual' do
-    get "/api/v1/invoice_items/#{@invoice_items[0].id}"
-    expect(response).to be_successful
-    invoice_item = JSON.parse(response.body)
+      expect(data['data']['attributes']['id']).to eq(@invoice.id.to_s)
+    end
 
-    expect(invoice_item['data']['attributes']['quantity']).to eq(@invoice_items[0].quantity)
-    expect(invoice_item['data']['id']).to eq(@invoice_items[0].id.to_s)
+    it 'can get items' do
+      get "/api/v1/invoice_items/#{@invoice_item.id}/item"
+
+      expect(response).to be_successful
+      data = JSON.parse(response.body)
+
+      expect(data['data']['attributes']['id']).to eq(@item.id.to_s)
+    end
   end
 
   it 'can find_by' do
