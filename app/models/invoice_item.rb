093115192150c311dec562358baa04560ebb173d
msimon42 <msimon@uab.edu>
2020-01-30 11:54:25 -0700

    Add method that finds invoice_item and returns one of its parents

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,9 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
+
+  def self.find_and_return_parent(id, attr)
+    invoice_item = find(id)
+    invoice_item.send(attr)
+  end
 end
