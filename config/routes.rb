f3777583d6e772f794c39168f4e4647d45df51a0
msimon42 <msimon@uab.edu>
2020-01-29 17:38:51 -0700

    Add routes for find_by

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'merchants/most_revenue', to: 'merchants#most_revenue'
+      get '/merchants/most_revenue', to: 'merchants#most_revenue'
+
+      get '/invoices/find', to: 'invoices#find'
+      get '/items/find', to: 'items#find'
+      get '/transactions/find', to: 'transactions#find'
+      get '/invoice_items/find', to: 'invoice_items#find'
+      get '/customers/find', to: 'customers#find'
+      get '/merchants/find', to: 'merchants#find'
 
       resources :customers, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
