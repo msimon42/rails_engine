20e654f7296d1b5ea4c21ced7e55538b64fabdc3
msimon42 <msimon@uab.edu>
2020-01-30 13:37:12 -0700

    Add tests for invoice relationships

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -1,26 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'invoices api' do
-  before :each do
-    @invoices = create_list :invoice, 10
+  describe 'index show' do
+    before :each do
+      @invoices = create_list :invoice, 10
+    end
+
+    it 'can get all invoices' do
+      get "/api/v1/invoices"
+
+      expect(response).to be_successful
+      invoices = JSON.parse(response.body)
+
+      expect(@invoices.length).to eq(10)
+    end
+
+    it 'can get an individual' do
+      get "/api/v1/invoices/#{@invoices[0].id}"
+      expect(response).to be_successful
+      invoice = JSON.parse(response.body)
+
+      expect(invoice['data']['attributes']['status']).to eq(@invoices[0].status)
+      expect(invoice['data']['id']).to eq(@invoices[0].id.to_s)
+    end
   end
 
-  it 'can get all invoices' do
-    get "/api/v1/invoices"
-
-    expect(response).to be_successful
-    invoices = JSON.parse(response.body)
-
-    expect(@invoices.length).to eq(10)
-  end
-
-  it 'can get an individual' do
-    get "/api/v1/invoices/#{@invoices[0].id}"
-    expect(response).to be_successful
-    invoice = JSON.parse(response.body)
-
-    expect(invoice['data']['attributes']['status']).to eq(@invoices[0].status)
-    expect(invoice['data']['id']).to eq(@invoices[0].id.to_s)
+  describe 'relationships' do
+    before :each do
+      @customer = create :customer
+      @merchant = create :merchant
   end
 
   it 'can find_by' do
