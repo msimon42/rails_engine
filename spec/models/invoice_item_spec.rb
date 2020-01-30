9330eef6d56f8632f3ea0b603326c036c79114d9
msimon42 <msimon@uab.edu>
2020-01-30 11:53:47 -0700

    Add test for find_and_return_parent

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe InvoiceItem, type: :model do
     it {should belong_to(:invoice)}
     it {should belong_to(:item)}
   end
+
+  describe 'methods' do
+    before :each do
+      @item = create :item
+      @invoice = create :invoice
+      @invoice_item = create :invoice_item, item: @item, invoice: @invoice
+    end
+
+    it 'find_and_return_parent' do
+      invoice = InvoiceItem.find_and_return_parent(@invoice_item.id.to_s, 'invoice')
+      expect(invoice.id).to eq(@invoice.id)
+    end
+  end
 end
