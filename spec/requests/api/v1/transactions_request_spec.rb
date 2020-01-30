b68f45b6247e5d766f0bdbdfc11eb7f2756180f9
msimon42 <msimon@uab.edu>
2020-01-29 18:04:49 -0700

    Add test for transaction find all

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'transactions api' do
     expect(response).to be_successful
     transactions = JSON.parse(response.body)
 
-    expect(@transactions.length).to eq(10)
+    expect(transactions.length).to eq(10)
   end
 
   it 'can get an individual' do
@@ -30,5 +30,17 @@ RSpec.describe 'transactions api' do
     expect(response).to be_successful
     data = JSON.parse(response.body)
     expect(data['data']['id']).to eq(transaction.id.to_s)
+  end
+
+  it 'can find all' do
+    transaction_1 = create :transaction, credit_card_number: '1829493918374983'
+    transaction_2 = create :transaction, credit_card_number: '1829493918374983'
+    transaction_3 = create :transaction, credit_card_number: '1829493918374983'
+    transaction_4 = create :transaction, credit_card_number: '1829534345664456'
+
+    get "/api/v1/transactions/find_all?credit_card_number=1829493918374983"
+    expect(response).to be_successful
+    data = JSON.parse(response.body)
+    expect(data.length).to eq(3)
   end
 end
