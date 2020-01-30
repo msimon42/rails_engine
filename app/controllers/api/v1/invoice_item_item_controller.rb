5bbb43147a2c52cb1d6061bdc815d98b52e70ad4
msimon42 <msimon@uab.edu>
2020-01-30 13:37:20 -0700

    Remove pry

--- app/controllers/api/v1/invoice_item_item_controller.rb
+++ app/controllers/api/v1/invoice_item_item_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::InvoiceItemItemController < ApplicationController
   def show
     item = InvoiceItem.find_and_return_parent(params[:invoice_item_id], 'item')
-    binding.pry
     render json: ItemSerializer.new(item)
   end
 end
