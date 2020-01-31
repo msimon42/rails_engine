b0eff33f8ca55c8292796777915343d2b7ff7a22
msimon42 <msimon@uab.edu>
2020-01-30 23:58:50 -0700

    Add method to get total revenue by date

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,11 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :transactions
   has_many :items, through: :invoice_items
+
+  def self.revenue_by_date(date)
+    where(created_at: Time.zone.parse(date)..Time.zone.parse(date).next_day)
+      .joins(:invoice_items, :transactions)
+      .merge(Transaction.successful)
+      .sum('quantity * unit_price')
+  end
 end
