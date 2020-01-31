29d68f67f1438e5f6bb95e1eebe443dafd783cef
msimon42 <msimon@uab.edu>
2020-01-30 23:56:53 -0700

    Create test for revenue_by_date

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -37,5 +37,35 @@ RSpec.describe Merchant, type: :model do
       expect(Merchant.top_n_by_revenue(2).first).to eq(merchants[2])
       expect(Merchant.top_n_by_revenue(2).last).to eq(merchants[1])
     end
+
+    it 'revenue_by_date' do
+      merchants = create_list :merchant, 3
+
+      invoice_1 = create :invoice, merchant: merchants[0]
+      invoice_2 = create :invoice, merchant: merchants[0]
+      invoice_3 = create :invoice, merchant: merchants[1]
+      invoice_4 = create :invoice, merchant: merchants[1]
+      invoice_5 = create :invoice, merchant: merchants[2]
+      invoice_6 = create :invoice, merchant: merchants[2]
+
+      create :invoice_item, invoice: invoice_1, quantity: 2, unit_price: 300
+      create :invoice_item, invoice: invoice_2, quantity: 3, unit_price: 500
+      create :invoice_item, invoice: invoice_2, quantity: 1, unit_price: 25
+      create :invoice_item, invoice: invoice_3, quantity: 6, unit_price: 10000
+      create :invoice_item, invoice: invoice_4, quantity: 8, unit_price: 15000
+      create :invoice_item, invoice: invoice_6, quantity: 10, unit_price: 300000
+      create :invoice_item, invoice: invoice_5, quantity: 2, unit_price: 30000
+
+      create :transaction, invoice: invoice_1, created_at: Time.new
+      create :transaction, invoice: invoice_2, created_at: Time.new
+      create :transaction, invoice: invoice_3, created_at: Time.new
+      create :transaction, invoice: invoice_4, created_at: Time.new
+      create :transaction, invoice: invoice_5, created_at: Time.parse('2019-12-14')
+      create :transaction, invoice: invoice_6, created_at: Time.parse('2019-12-15')
+
+      expect(Merchant.revenue_by_date(Time.new.to_s).length).to eq(4)
+
+
+    end
   end
 end
