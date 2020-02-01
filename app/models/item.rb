35a0e799ea29827fce9034bd863ec5a9904d10ea
msimon42 <msimon@uab.edu>
2020-02-01 16:15:38 -0700

    Add method to get top items by revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
+
+  def self.top_n_by_revenue(n)
+    select('items.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+        .joins(:invoice_items, :transactions)
+        .merge(Transaction.successful)
+        .group(:id)
+        .order('revenue DESC')
+        .limit(n)
+  end
 end
