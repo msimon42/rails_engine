8aed605f871e041f63a41f4e016615f1d0735e9f
msimon42 <msimon@uab.edu>
2020-02-01 16:16:51 -0700

    Add method to get top customer by merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,14 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.rank_by_merchant(id)
+    select('customers.*, count(transactions.id) AS total_successful_transactions')
+      .joins(:transactions)
+      .merge(Transaction.successful)
+      .where('invoices.merchant_id = ?', id)
+      .group(:id)
+      .order('total_successful_transactions DESC')
+      .limit(1)
+  end
 end
