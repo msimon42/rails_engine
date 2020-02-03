58c8f1582a0107304d45213b66d1e5e7e46183e2
msimon42 <msimon@uab.edu>
2020-02-02 18:44:29 -0700

    Add id to serializer

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,4 +1,4 @@
 class TransactionSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :invoice_id, :credit_card_number, :result
+  attributes :id, :invoice_id, :credit_card_number, :result
 end
